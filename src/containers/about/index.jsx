5967fbbf34018895d2c84835eb778c56035e7dde
UzmaTabassum <uzmatabassumshaik@gmail.com>
2023-10-16 02:12:45 +0530

    first commit

---
+++ src/containers/about/index.jsx
@@ -0,0 +1,116 @@
+import React from "react";
+import { BsInfoCircleFill } from "react-icons/bs";
+import PageHeaderContent from "../../components/pageHeaderContent";
+import { Animate } from "react-simple-animate";
+import "./styles.scss";
+import { DiApple, DiAndroid } from "react-icons/di";
+import { FaDev, FaDatabase } from "react-icons/fa";
+
+const personalDetails = [
+  {
+    label: "Name",
+    value: "Uzma Tabassum Shaik",
+  },
+  {
+    label: "Age",
+    value: "19",
+  },
+  {
+    label: "Address",
+    value: "India",
+  },
+  {
+    label: "Email",
+    value: "uzmatabassumshaik@gmail.com",
+  },
+  {
+    label: "Contact No",
+    value: "7842100167",
+  },
+];
+
+const jobSummary =
+  "Currently pursuing my Bachelor of Technology degree at Vellore Institute of Technology-AP University.Oriented and driven Computer Science and Engineering student with a passion for Web Development and other programming languages.";
+  
+
+const About = () => {
+  return (
+    <section id="about" className="about">
+      <PageHeaderContent
+        headerText="About Me"
+        icon={<BsInfoCircleFill size={40} />}
+      />
+      <div className="about__content">
+        <div className="about__content__personalWrapper">
+          <Animate
+            play
+            duration={1.5}
+            delay={1}
+            start={{
+              transform: "translateX(-900px)",
+            }}
+            end={{
+              transform: "translatex(0px)",
+            }}
+          >
+            <h3>Front End Developer</h3>
+            <p>{jobSummary}</p>
+          </Animate>
+
+          <Animate
+            play
+            duration={1.5}
+            delay={1}
+            start={{
+              transform: "translateX(500px)",
+            }}
+            end={{
+              transform: "translatex(0px)",
+            }}
+          >
+            <h3 className="personalInformationHeaderText">
+              Personal Information
+            </h3>
+            <ul>
+              {personalDetails.map((item, i) => (
+                <li key={i}>
+                  <span className="title">{item.label}</span>
+                  <span className="value">{item.value}</span>
+                </li>
+              ))}
+            </ul>
+          </Animate>
+        </div>
+        <div className="about__content__servicesWrapper">
+          <Animate
+            play
+            duration={1.5}
+            delay={1}
+            start={{
+              transform: "translateX(600px)",
+            }}
+            end={{
+              transform: "translatex(0px)",
+            }}
+          >
+            <div className="about__content__servicesWrapper__innerContent">
+              <div>
+                <FaDev size={60} color="var( --yellow-theme-main-color)" />
+              </div>
+              <div>
+                <DiAndroid size={60} color="var( --yellow-theme-main-color)" />
+              </div>
+              <div>
+                <FaDatabase size={60} color="var( --yellow-theme-main-color)" />
+              </div>
+              <div>
+                <DiApple size={60} color="var( --yellow-theme-main-color)" />
+              </div>
+            </div>
+          </Animate>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default About;
---
+++ src/containers/resume/utils.js
@@ -0,0 +1,42 @@
+export const data = {
+    experience: [
+      {
+        title: "Consultant",
+        subTitle: "Miami, FL",
+        description:
+          " User Experience, Visual Design",
+      },
+      {
+        title: "Senior Consultant",
+        subTitle: "London, UK",
+        description:
+          "Web development, Databases, Backend developement",
+      },
+      {
+        title: "Project Manager",
+        subTitle: "Bangalore, INDIA",
+        description:
+          "Creative Direction Project Management, Team Leading",
+      },
+    ],
+    education: [
+      {
+        title: "Secondary",
+        subTitle: "Narayana School",
+        description:
+          "10th Grade",
+      },
+      {
+        title: "Higher Secondary",
+        subTitle: "Narayana College",
+        description:
+          "12th Grade in Science",
+      },
+      {
+        title: "B.Tech",
+        subTitle: "VIT institute of technology",
+        description:
+          "Bachelor of technology in computer science",
+      },
+    ],
+  };
